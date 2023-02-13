239ad4344dac32b70d98e8d67ee0149cfb530d40
seyun <68141564+Seyun-0106@users.noreply.github.com>
2023-02-13 21:21:42 +0900

    Create Q7.java

---
+++ 기본/Q7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("n:");
+		int n = scn.nextInt();
+		
+		int sum = 0;
+		for(int i=1; i<=n; i++){
+		    sum += i;
+		}
+		System.out.print("sum=" + sum);
+		
+		
+	}
+}
