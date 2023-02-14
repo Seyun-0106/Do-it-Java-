92db85a0ba5c0444b951f2ac20ebb96625f90c59
seyun <68141564+Seyun-0106@users.noreply.github.com>
2023-02-14 21:15:49 +0900

    Create Q16.java

---
+++ 기본/Q16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void spira(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<i*2+1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+	public static void main(String[] args) {
+	    Scanner scn = new Scanner(System.in);
+	    System.out.print("n = ");
+	    int n = scn.nextInt();
+	    spira(n);
+	}
+}
